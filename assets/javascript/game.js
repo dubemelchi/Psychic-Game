bce3a35fb59c36f96646a687718b557bb14cc2e2
dubemelchi <dubemelchi@gmail.com>
2019-05-27 04:26:22 -0400

    setup variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+/*
+Game Functions:
+- Player must guess a letter.
+- Player gets a certain number of guesses.
+- Notify player of guesses remaining.
+- Notify plater of correct answer if loose.
+- Let player choose to play again.
+
+*/
+/*My id's and classes: id = #game, #ws, #ls, #gLeft, #gSoFar, 
+classes: .firstLetter, .lastLetter, .container,
+ */
+// Game values
+
+
+let alphabet = ['a','b','c','d','e','f','g','h','j'];
+
+let wins = 0,
+    losses = 0,
+    guessesLeft = 3,
+    yourGuesses = 'a';
+
+    
+
+
+
+
+
+
+
+
+      
+
+
